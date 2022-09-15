e5a12cd3934f2ed8a07011039c2652b0a2c4cb67
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2022-09-15 22:59:52 +0200

    Nav functionality
    Added funvtionality for nav in mobile and tablet view.

--- .github/Kod Rebecka/script.js
+++ .github/Kod Rebecka/script.js
@@ -1,4 +1,7 @@
 function removeCookies() {
     document.getElementById("cookies").style.display = "none";
-  }
+  };
 
+  function showNav(){
+    document.getElementById("nav").style.display = "none";
+  }
