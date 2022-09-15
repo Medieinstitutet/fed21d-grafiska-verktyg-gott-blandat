5715f8f11f937dd253d4b5655694cfc4b8c66142
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2022-09-15 23:24:28 +0200

    Design and toggle nav
    Design nav for mobile is done, also added toogle function for nav.

--- .github/Kod Rebecka/script.js
+++ .github/Kod Rebecka/script.js
@@ -1,7 +1,16 @@
+
+
 function removeCookies() {
     document.getElementById("cookies").style.display = "none";
   };
 
-  function showNav(){
-    document.getElementById("nav").style.display = "none";
-  }
+
+function showNav(){
+    let nav = document.getElementById("nav")
+    if(nav.style.display === "none"){
+        nav.style.display = "block";
+    }
+    else{
+        nav.style.display = "none";
+    }
+  };
