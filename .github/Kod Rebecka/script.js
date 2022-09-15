ee4cb2e4f11bda56c13f3888eb87c9bb6e4ffdba
Rebecka Larsson <94102338+Reebeecka@users.noreply.github.com>
2022-09-15 23:56:48 +0200

    Hamburger toggle
    Toggle hamburger from lines to X and back

--- .github/Kod Rebecka/script.js
+++ .github/Kod Rebecka/script.js
@@ -1,5 +1,3 @@
-
-
 function removeCookies() {
     document.getElementById("cookies").style.display = "none";
   };
@@ -7,10 +5,24 @@ function removeCookies() {
 
 function showNav(){
     let nav = document.getElementById("nav")
+    let line1 = document.getElementById("line1");
+    let line2 = document.getElementById("line2");
+    let line3 = document.getElementById("line3");
+
     if(nav.style.display === "block"){
         nav.style.display = "none";
+        line2.style.display = "block";
+        line1.style.top = "25px"
+        line1.style.transform = "rotate(0deg)"
+        line3.style.top = "49px"
+        line3.style.transform = "rotate(0deg)"
     }
     else{
         nav.style.display = "block";
+        line2.style.display = "none";
+        line1.style.top = "37px"
+        line1.style.transform = "rotate(45deg)"
+        line3.style.top = "37px"
+        line3.style.transform = "rotate(-45deg)"
     }
   };
